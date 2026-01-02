71d3a44753cd5df1eaa387231336e6e8214cbeac
Rolando Cruz <rolandocrzprz@gmail.com>
2026-01-02 16:43:33 -0700

    feat: add function toLowerCase for email and create and login user

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -1,4 +1,10 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity('users')
 export class User {
@@ -19,4 +25,14 @@ export class User {
 
   @Column('text', { array: true, default: ['user'] })
   roles: string[];
+
+  @BeforeInsert()
+  checkFieldBeforeInsert() {
+    this.email = this.email.toLowerCase().trim();
+  }
+
+  @BeforeUpdate()
+  checkFieldBeforeUpdate() {
+    this.checkFieldBeforeInsert();
+  }
 }
